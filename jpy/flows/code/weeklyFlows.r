9ed269b8ba9718bdfc99723e0ec9bea2e3f9b6b9
mcooganj <mcooganj@gmail.com>
2013-05-11 06:14:02 +1000

    'done get weekly MOF data'

---
+++ jpy/flows/code/weeklyFlows.r
@@ -0,0 +1,58 @@
+# get the ABS file and make some labour market charts
+rm(list=ls()); gc()
+Sys.setenv(TZ = 'GMT')
+#
+require(gdata)
+require(data.table)
+require(xts)
+require(ggplot2)
+require(reshape2)
+require(TTR)
+require(gridExtra)
+require(timsac)
+
+## get from web or saved xls?
+getWeb <- TRUE
+
+## {{{ PATH stuff
+projectPATH <- "~/r/jpy/flows"
+plotPATH <- file.path(projectPATH, "pics")
+codePATH <- file.path(projectPATH, "code")
+dataPATH <- file.path(projectPATH, "data")
+# }}}
+
+if (getWeb)
+{
+    MOF_weeklycsv <- "http://www.mof.go.jp/international_policy/reference/itn_transactions_in_securities/week.csv"
+#
+    MOF_data <- read.csv(MOF_weeklycsv,
+                         header = FALSE,
+                         stringsAsFactors = FALSE,
+                         fileEncoding = 'latin1',
+                         skip = 16
+                         )
+#
+    MOF_data <- MOF_data[, 1:23]
+    MOF_data[, 1] <- seq(as.Date("2005-01-08"), by = 'week', length.out = nrow(MOF_data))
+    MOF_data[, -1] <- as.data.frame(lapply(MOF_data[,-1],  # convert to numbers
+        function(d) type.convert(gsub(d, pattern=",", replace=""))))
+#
+    names(MOF_data) <- c('date',
+                         'JIA_eq.sales', 'JIA_eq.purch', 'JIA_eq.net',
+                         'JIA_bond.sales', 'JIA_bond.purch', 'JIA_bond.net',
+                         'JIA_eqBond.subTtl',
+                         'JIA_mmkt.sales', 'JIA_mmkt.purch', 'JIA_mmkt.net',
+                         'JIA_total',
+                         'FIJ_eq.purch', 'FIJ_eq.sales', 'FIJ_eq.net',
+                         'FIJ_bonds.purch', 'FIJ_bonds.sales', 'FIJ_bonds.net',
+                         'FIJ_eqBond.subTtl',
+                         'FIJ_mmkt.purch', 'FIJ_mmkt.sales', 'FIJ_mmkt.net',
+                         'FIJ_total'
+                         )
+    MOF_data[, 2:23] <- MOF_data[, 2:23] / 10 # native unit is Y100m => converted to Ybn
+    save(MOF_data, file = file.path(dataPATH, "weeklyMOF.rdata"))
+
+# +ve net = Japan money inflows to JPY => +ve JIA net is sales > purch: +FIJ is purch > sales
+} else {
+    load(file.path(dataPATH, "weeklyMOF.rdata"))
+}
