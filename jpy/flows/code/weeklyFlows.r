ef8e6367a428d89cf48cb0dd058523a81b925a2e
mcooganj <mcooganj@gmail.com>
2013-05-11 11:02:51 +1000

    'japanese flows, including charts -- wk and mon'

--- jpy/flows/code/weeklyFlows.r
+++ jpy/flows/code/weeklyFlows.r
@@ -15,6 +15,7 @@ require(TTR)
 require(gridExtra)
 require(timsac)
 source("~/r/Rhelpers/helperFuncts.r")
+source("~/r/Rhelpers/RAcolorpal.r")
 # }}}
 
 ## {{{ PATH stuff
@@ -27,7 +28,7 @@ dataPATH <- file.path(projectPATH, "data")
 # get from web or saved xls?
 getWeb <- FALSE
 
-## {{{ get the data
+## {{{ get the dataza
 if (getWeb)
 {
     MOF_weeklycsv <- "http://www.mof.go.jp/international_policy/reference/itn_transactions_in_securities/week.csv"
@@ -49,12 +50,12 @@ if (getWeb)
                          'JIA_bond.sales', 'JIA_bond.purch', 'JIA_bond.net',
                          'JIA_eqBond.subTtl',
                          'JIA_mmkt.sales', 'JIA_mmkt.purch', 'JIA_mmkt.net',
-                         'JIA_total',
+                         'JIA_total.net',
                          'FIJ_eq.purch', 'FIJ_eq.sales', 'FIJ_eq.net',
-                         'FIJ_bonds.purch', 'FIJ_bonds.sales', 'FIJ_bonds.net',
+                         'FIJ_bond.purch', 'FIJ_bond.sales', 'FIJ_bond.net',
                          'FIJ_eqBond.subTtl',
                          'FIJ_mmkt.purch', 'FIJ_mmkt.sales', 'FIJ_mmkt.net',
-                         'FIJ_total'
+                         'FIJ_total.net'
                          )
     MOF_data[, 2:23] <- MOF_data[, 2:23] / 10 # native unit is Y100m => converted to Ybn
     save(MOF_data, file = file.path(dataPATH, "weeklyMOF.rdata"))
@@ -64,8 +65,276 @@ if (getWeb)
     load(file.path(dataPATH, "weeklyMOF.rdata"))
 }
 
+MOF_datax <- xtsF(MOF_data)
+MOF_data_m <- apply.monthly(MOF_datax, colSums)
+MOFm_melt <- meltx(MOF_data_m)
+MOF_melt <- melt(tail(MOF_data, 16), id.vars = 1)
+
 # }}}
 
 ## {{{ PLOTS
 
-gp_MOFnet <- ggplot(data = MOF_data)
+# {{{ montly data
+# Japan trading foreign equities
+gp_JIA.eq <- ggplot(data = subset(MOFm_melt, variable %in% c('JIA_eq.sales',
+                                                            'JIA_eq.purch',
+                                                            'JIA_eq.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Japanese foreign equity flows: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "jia_eq.png"))
+grid.arrange(gp_JIA.eq, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading foreign bonds
+gp_JIA.bond <- ggplot(data = subset(MOFm_melt, variable %in% c('JIA_bond.sales',
+                                                              'JIA_bond.purch',
+                                                              'JIA_bond.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Japanese foreign bond flows: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "jia_bond.png"))
+grid.arrange(gp_JIA.bond, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading foreign mmkt
+gp_JIA.mmkt <- ggplot(data = subset(MOFm_melt, variable %in% c('JIA_mmkt.sales',
+                                                              'JIA_mmkt.purch',
+                                                              'JIA_mmkt.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Japanese foreign money market flows: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "jia_mmkt.png"))
+grid.arrange(gp_JIA.mmkt, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading net foreign
+gp_JIA.nets <- ggplot(data = subset(MOFm_melt, variable %in% c('JIA_eq.net',
+                                                              'JIA_bond.net',
+                                                              'JIA_mmkt.net',
+                                                              'JIA_total.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Net Japanese foreign investment flows: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_4, guide = 'none') +
+                    scale_fill_manual(values = RAPal_4, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "jia_nets.png"))
+grid.arrange(gp_JIA.nets, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# foreign trading japanese equities
+gp_FIJ.eq <- ggplot(data = subset(MOFm_melt, variable %in% c('FIJ_eq.sales',
+                                                            'FIJ_eq.purch',
+                                                            'FIJ_eq.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Foreign flows in Japanese equities: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "fij_eq.png"))
+grid.arrange(gp_FIJ.eq, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# foreign trading JGBs
+gp_FIJ.bond <- ggplot(data = subset(MOFm_melt, variable %in% c('FIJ_bond.sales',
+                                                              'FIJ_bond.purch',
+                                                              'FIJ_bond.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Foreign flows in JGBs: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "fij_bond.png"))
+grid.arrange(gp_FIJ.bond, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# foreign trading JPY mmkt
+gp_FIJ.mmkt <- ggplot(data = subset(MOFm_melt, variable %in% c('FIJ_mmkt.sales',
+                                                              'FIJ_mmkt.purch',
+                                                              'FIJ_mmkt.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Foreign flows in JPY mmkt: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "fij_mmkt.png"))
+grid.arrange(gp_FIJ.mmkt, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading net foreign
+gp_FIJ.nets <- ggplot(data = subset(MOFm_melt, variable %in% c('FIJ_eq.net',
+                                                              'FIJ_bond.net',
+                                                              'FIJ_mmkt.net',
+                                                              'FIJ_total.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Net foreign flows in Japanese securities: JPYbn/mth") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_4, guide = 'none') +
+                    scale_fill_manual(values = RAPal_4, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week", "wk2mon",  "fij_nets.png"))
+grid.arrange(gp_FIJ.nets, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+# }}}
+
+# {{{ weekly data
+# Japan trading foreign equities
+gp_JIAwk.eq <- ggplot(data = subset(MOF_melt, variable %in% c('JIA_eq.sales',
+                                                            'JIA_eq.purch',
+                                                            'JIA_eq.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Japanese foreign equity flows: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "jia_eqWk.png"))
+grid.arrange(gp_JIAwk.eq, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading foreign bonds
+gp_JIAwk.bond <- ggplot(data = subset(MOF_melt, variable %in% c('JIA_bond.sales',
+                                                              'JIA_bond.purch',
+                                                              'JIA_bond.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Japanese foreign bond flows: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "jia_bondWk.png"))
+grid.arrange(gp_JIAwk.bond, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading foreign mmkt
+gp_JIAwk.mmkt <- ggplot(data = subset(MOF_melt, variable %in% c('JIA_mmkt.sales',
+                                                              'JIA_mmkt.purch',
+                                                              'JIA_mmkt.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Japanese foreign money market flows: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "jia_mmktWk.png"))
+grid.arrange(gp_JIAwk.mmkt, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading net foreign
+gp_JIAwk.nets <- ggplot(data = subset(MOF_melt, variable %in% c('JIA_eq.net',
+                                                              'JIA_bond.net',
+                                                              'JIA_mmkt.net',
+                                                              'JIA_total.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Net Japanese foreign investment flows: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_4, guide = 'none') +
+                    scale_fill_manual(values = RAPal_4, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "jia_netsWk.png"))
+grid.arrange(gp_JIAwk.nets, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# foreign trading japanese equities
+gp_FIJwk.eq <- ggplot(data = subset(MOF_melt, variable %in% c('FIJ_eqwk.sales',
+                                                            'FIJ_eq.purch',
+                                                            'FIJ_eq.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Foreign flows in Japanese equities: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "fij_eqWk.png"))
+grid.arrange(gp_FIJwk.eq, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# foreign trading JGBs
+gp_FIJwk.bond <- ggplot(data = subset(MOF_melt, variable %in% c('FIJ_bond.sales',
+                                                              'FIJ_bond.purch',
+                                                              'FIJ_bond.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Foreign flows in JGBs: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "fij_bondWk.png"))
+grid.arrange(gp_FIJwk.bond, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# foreign trading JPY mmkt
+gp_FIJwk.mmkt <- ggplot(data = subset(MOF_melt, variable %in% c('FIJ_mmkt.sales',
+                                                              'FIJ_mmkt.purch',
+                                                              'FIJ_mmkt.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Foreign flows in JPY mmkt: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_3, guide = 'none') +
+                    scale_fill_manual(values = RAPal_3, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "fij_mmktWk.png"))
+grid.arrange(gp_FIJwk.mmkt, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+# japan trading net foreign
+gp_FIJwk.nets <- ggplot(data = subset(MOF_melt, variable %in% c('FIJ_eq.net',
+                                                              'FIJ_bond.net',
+                                                              'FIJ_mmkt.net',
+                                                              'FIJ_total.net')),
+                    aes(x = date, y = value, color = variable, fill = variable)) +
+                    facet_grid(variable ~ ., scale = 'free_y') +
+                    labs(y = NULL, x = NULL) +
+                    labs(title = "Net foreign flows in Japanese securities: JPYbn/wk") +
+                    theme(legend.position = 'none') +
+                    scale_color_manual(values = RAPal_4, guide = 'none') +
+                    scale_fill_manual(values = RAPal_4, guide = 'none') +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "week",  "fij_netsWk.png"))
+grid.arrange(gp_FIJwk.nets, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+# }}}
+
