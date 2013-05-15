7a8166a3ca3ab9f48d9839e40e9f79effec3f3b2
mcooganj <mcooganj@gmail.com>
2013-05-16 05:41:03 +1000

    'start of a tot / RBA trade prices setup'

---
+++ AUD/bop/tot/code/tot.r
@@ -0,0 +1,19 @@
+
+# {{{ packages and functions
+require(ggplot2)
+require(reshape2)
+require(xts)
+source('~/R/Rhelpers/helperFuncts.r')
+source('~/R/Rhelpers/RAcolorpal.r')
+# }}}
+
+# {{{ PATHS
+projectPATH <- "~/R/aud/bop/tot/"
+codePATH <- file.path(projectPATH, "code")
+dataPATH <- file.path(projectPATH, "data")
+plotPATH <- file.path(projectPATH, "plot")
+# }}}
+
+
+
+
