875cbd9b81286705822cddafe8a12eade665d76a
mcooganj <mcooganj@gmail.com>
2013-05-22 06:12:47 +1000

    'now with a 2 col'

---
+++ Rhelpers/RAcolorpal.r
@@ -0,0 +1,11 @@
+
+require(RColorBrewer)
+# color palette
+RAPal_2 <- c('red', 'blue')
+RAPal_3 <- c('red', 'orange', 'blue')
+RAPal_4 <- c('red', 'orange', 'green', 'blue')
+RAPal_5 <- brewer.pal(5, 'RdYlBu')
+RApal_9 <- brewer.pal(9, "RdYlBu")
+RApal_10 <- brewer.pal(10, "RdYlBu")
+RApal_front5 <- brewer.pal(5, "RdYlBu")[1:5]
+RApal_back5 <- brewer.pal(10, "RdYlBu")[6:10]
