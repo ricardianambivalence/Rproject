0eb32ab64424103c6567499328f40ef811aa44ce
mcooganj <mcooganj@gmail.com>
2013-05-26 23:04:43 +1000

    'done RnD'

--- AUD/rnd/code/rnd.r
+++ AUD/rnd/code/rnd.r
@@ -39,7 +39,51 @@ appldRnD$ppn <- appldRnD$appliedResearch / appldRnD$appliedResearch[13]
 experimentalRnD <- rndXLS[, c('sector', 'experimentalDev')]
 experimentalRnD$ppn <- experimentalRnD$experimentalDev / experimentalRnD$experimentalDev[13]
 
-experimentalRnD <- rndXLS[, c('sector', 'totalRnD')]
-experimentalRnD$ppn <- experimentalRnD$totalRnD / experimentalRnD$totalRnD[13]
+totalRnD <- rndXLS[, c('sector', 'totalRnD')]
+totalRnD$ppn <- totalRnD$totalRnD / totalRnD$totalRnD[13]
 
+gp_basic <- ggplot(data = basicRnD[-13,], aes(x = sector, y = 100*ppn, fill = sector)) +
+                    theme(axis.text.x=element_text(angle = 90, hjust = 1)) +
+                    labs(title = "Basic RnD: percent of total BERD") +
+                    labs(y = NULL, x = NULL) +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "basicRnD.png"))
+grid.arrange(gp_basic, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+gp_stratBasic <- ggplot(data = stratBasicRnD[-13,], aes(x = sector, y = 100*ppn, fill = sector)) +
+                    theme(axis.text.x=element_text(angle = 90, hjust = 1)) +
+                    labs(title = "Strategic Basic RnD: percent of total BERD") +
+                    labs(y = NULL, x = NULL) +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "stratbasicRnD.png"))
+grid.arrange(gp_stratBasic, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+gp_appld <- ggplot(data = appldRnD[-13,], aes(x = sector, y = 100*ppn, fill = sector)) +
+                    theme(axis.text.x=element_text(angle = 90, hjust = 1)) +
+                    labs(title = "Applied Research: percent of total BERD") +
+                    labs(y = NULL, x = NULL) +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "appliedRnD.png"))
+grid.arrange(gp_appld, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+gp_experimntl <- ggplot(data = experimentalRnD[-13,], aes(x = sector, y = 100*ppn, fill = sector)) +
+                    theme(axis.text.x=element_text(angle = 90, hjust = 1)) +
+                    labs(title = "Experimental Development: percent of total BERD") +
+                    labs(y = NULL, x = NULL) +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "expDev.png"))
+grid.arrange(gp_experimntl, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
+
+gp_RnDTtl <- ggplot(data = totalRnD[-13,], aes(x = sector, y = 100*ppn, fill = sector)) +
+                    theme(axis.text.x=element_text(angle = 90, hjust = 1)) +
+                    labs(title = "Total Research and Development: percent of total BERD") +
+                    labs(y = NULL, x = NULL) +
+                    geom_bar(stat = 'identity')
+png(file.path(plotPATH, "totalRnD.png"))
+grid.arrange(gp_RnDTtl, sub = textGrob('www.ricardianambivalence.com'))
+dev.off()
 
