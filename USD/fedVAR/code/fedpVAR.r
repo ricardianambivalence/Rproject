cd2b552024ce5c33363c061c8826ea5b842085f8
mcooganj <mcooganj@gmail.com>
2013-06-13 03:22:46 +1000

    'changed to FFR, and made 3p and 4p vars'

--- USD/fedVAR/code/fedpVAR.r
+++ USD/fedVAR/code/fedpVAR.r
@@ -41,11 +41,12 @@ if(getData)
 }
 
 # make a single FFR instrument - use mid after 15 Dec 2008
-fedMid <- (DFEDTARU + DFEDTARL)/2
+fedMid <- (coredata(DFEDTARU) + coredata(DFEDTARL))/2
 names(fedMid) <- 'fedFunds'
-FFR <- rbind(DFEDTAR, fedMid)
+FFR <- xts(rbind(coredata(DFEDTAR), fedMid), order.by = c(index(DFEDTAR), index(DFEDTARU)))
 names(FFR) <- 'FFR'
-
+FFR <- apply.monthly(na.locf(FFR), last, na.rm=TRUE)
+index(FFR) <- dateSwitch(index(FFR), lastDay = FALSE)
 
 FCI <- apply.monthly(merge(NFCI, ANFCI), colMeans)
 index(FCI) <- dateSwitch(index(FCI), lastDay = FALSE)
@@ -109,19 +110,24 @@ icT <- "FPE"
 # {{{ arrange data sets
 # demand, FCI, 3m bill
 VAR3frame <- cbind(CFNAI, FCI$NFCI, FFR)
-VAR3frame_3m <- rollapplyr(na.locf(VAR3frame), 3, colMeans)['19820301::20090331']
+VAR3frame_3m <- rollapplyr(na.locf(VAR3frame), 3, colMeans)['19821101::20090331']
+
+# {{{ arrange data sets
+# demand, FCI, 3m bill
+VAR3pframe <- cbind(CFNAI, corePCE_6mAR, FFR)
+VAR3pframe_3m <- rollapplyr(na.locf(VAR3pframe), 3, colMeans)['19821101::20090331']
 
 # PPIe_shock, demand, FCI, 3m bill
 VAR4frame <- cbind(PPIe_shock, CFNAI, FCI$NFCI, FFR)
-VAR4frame_3m <- rollapplyr(na.locf(VAR4frame), 3, colMeans)['19820301::20090331']
+VAR4frame_3m <- rollapplyr(na.locf(VAR4frame), 3, colMeans)['19821101::20090331']
 
 # demand, FCI, cPCE, 3m bill
 VAR4pframe <- cbind(CFNAI, FCI$NFCI, corePCE_6mAR, FFR)
-VAR4pframe_3m <- rollapplyr(na.locf(VAR4pframe), 3, colMeans)['19820301::20090331']
+VAR4pframe_3m <- rollapplyr(na.locf(VAR4pframe), 3, colMeans)['19821101::20090331']
 
 # PPIe_shock, demand, FCI, inflation, 3m bill
 VAR5frame <- cbind(PPIe_shock, CFNAI, FCI$NFCI, corePCE_6mAR, FFR)
-VAR5frame_3m <- rollapplyr(na.locf(VAR5frame), 3, colMeans)['19820301::20090331']
+VAR5frame_3m <- rollapplyr(na.locf(VAR5frame), 3, colMeans)['19821101::20090331']
 # }}}close data arrange
 # ==> TODO -- email the dudes at the chicago fed about the FCI in a VAR ... adj or no?
 
@@ -143,6 +149,23 @@ fed3VAR.mod2 <- VAR((VAR3frame_3m), p = optLag3, ic = icT)
 fed3VAR.pp <- predict(fed3VAR.mod2, n.ahead = 60)
 fed3VAR.irf <- irf(fed3VAR.mod2, n.ahead=48)
 # }}} close 3 part VAR
+
+## {{{ three part VAR: with p ex oil FCI
+optLag3p <- findMaxVARLag(VAR3pframe_3m, firstMax = 9, crit = paste0(icT, "(n)"))
+
+# {{{VAR test stuff -- rmsfe etc
+fed3pVAR.test6 <- testVar(VAR3pframe_3m, skip = 94, nAhead = 6, Vlag = 6, IC = icT)
+sumTestError.fed3p_6m <- errTstVar(fed3pVAR.test6)
+# print(sumTestError.fed3_6m$r)
+
+fed3pVAR.test12 <- testVar(VAR3pframe_3m, skip = 94, nAhead = 12, Vlag = 9, IC = icT)
+sumTestError.fed3p_12m <- errTstVar(fed3pVAR.test12)
+# }}} close rmsfe
+fed3pVAR.mod <- VAR(scale(VAR3pframe_3m), p = optLag3, ic = icT)
+fed3pVAR.mod2 <- VAR((VAR3pframe_3m), p = optLag3, ic = icT)
+fed3pVAR.pp <- predict(fed3pVAR.mod2, n.ahead = 60)
+fed3pVAR.irf <- irf(fed3pVAR.mod2, n.ahead=48)
+# }}} close 3p part VAR
 
 ## {{{ four part VAR -- added oil shocks
 optLag4 <- findMaxVARLag(VAR4frame_3m, firstMax = 9, crit = paste0(icT, "(n)")) # 5
@@ -199,12 +222,12 @@ fed5VAR.irf <- irf(fed5VAR.mod2, n.ahead=48)
 ## note, as we have stopped estimation at Mar'09, need a way to thread new data into pred function
 # =-> 3 VAR
 pdf(file.path(plotPATH, "fed3VAR.pdf"))
-plot.zoo(fed3VAR.test6$tbill3m['1993::'],
+plot.zoo(fed3VAR.test6$FFR['1993::'],
          screen=1,
-         col=c(1, rep(8, ncol(fed3VAR.test6$tbill3m)-1)),
+         col=c(1, rep(8, ncol(fed3VAR.test6$FFR)-1)),
          las=1,
-         lwd = c(3, rep(1, ncol(fed3VAR.test6$tbill3m) - 1)),
-         type = c('s', rep('l', ncol(fed3VAR.test6$tbill3m) - 1)),
+         lwd = c(3, rep(1, ncol(fed3VAR.test6$FFR) - 1)),
+         type = c('s', rep('l', ncol(fed3VAR.test6$FFR) - 1)),
          main = "Pseudo Out of Sample (6m forecasts): 3 Part FED VAR",
          xlab = "",
          ylab = ""
@@ -215,12 +238,12 @@ dev.off()
 ## note, as we have stopped estimation at Mar'09, need a way to thread new data into pred function
 # =-> 4 VAR
 pdf(file.path(plotPATH, "fed4VAR.pdf"))
-plot.zoo(fed4VAR.test6$tbill3m['1993::'],
+plot.zoo(fed4VAR.test6$FFR['1993::'],
          screen=1,
-         col=c(1, rep(8, ncol(fed4VAR.test6$tbill3m)-1)),
+         col=c(1, rep(8, ncol(fed4VAR.test6$FFR)-1)),
          las=1,
-         lwd = c(3, rep(1, ncol(fed4VAR.test6$tbill3m) - 1)),
-         type = c('s', rep('l', ncol(fed4VAR.test6$tbill3m) - 1)),
+         lwd = c(3, rep(1, ncol(fed4VAR.test6$FFR) - 1)),
+         type = c('s', rep('l', ncol(fed4VAR.test6$FFR) - 1)),
          main = "Pseudo Out of Sample (6m forecasts): 4 Part FED VAR",
          xlab = "",
          ylab = ""
@@ -231,12 +254,12 @@ dev.off()
 ## note, as we have stopped estimation at Mar'09, need a way to thread new data into pred function
 # =-> 4p VAR
 pdf(file.path(plotPATH, "fed4pVAR.pdf"))
-plot.zoo(fed4pVAR.test6$tbill3m['1993::'],
+plot.zoo(fed4pVAR.test6$FFR['1993::'],
          screen=1,
-         col=c(1, rep(8, ncol(fed4pVAR.test6$tbill3m)-1)),
+         col=c(1, rep(8, ncol(fed4pVAR.test6$FFR)-1)),
          las=1,
-         lwd = c(3, rep(1, ncol(fed4pVAR.test6$tbill3m) - 1)),
-         type = c('s', rep('l', ncol(fed4pVAR.test6$tbill3m) - 1)),
+         lwd = c(3, rep(1, ncol(fed4pVAR.test6$FFR) - 1)),
+         type = c('s', rep('l', ncol(fed4pVAR.test6$FFR) - 1)),
          main = "Pseudo Out of Sample (6m forecasts): 4p Part FED VAR",
          xlab = "",
          ylab = ""
@@ -247,12 +270,12 @@ dev.off()
 ## note, as we have stopped estimation at Mar'09, need a way to thread new data into pred function
 # =-> 5 VAR
 pdf(file.path(plotPATH, "fed5VAR.pdf"))
-plot.zoo(fed5VAR.test6$tbill3m['1993::'],
+plot.zoo(fed5VAR.test6$FFR['1993::'],
          screen=1,
-         col=c(1, rep(8, ncol(fed5VAR.test6$tbill3m)-1)),
+         col=c(1, rep(8, ncol(fed5VAR.test6$FFR)-1)),
          las=1,
-         lwd = c(3, rep(1, ncol(fed5VAR.test6$tbill3m) - 1)),
-         type = c('s', rep('l', ncol(fed5VAR.test6$tbill3m) - 1)),
+         lwd = c(3, rep(1, ncol(fed5VAR.test6$FFR) - 1)),
+         type = c('s', rep('l', ncol(fed5VAR.test6$FFR) - 1)),
          main = "Pseudo Out of Sample (6m forecasts): 5 Part FED VAR",
          xlab = "",
          ylab = ""
