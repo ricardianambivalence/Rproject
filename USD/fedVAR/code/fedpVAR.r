8c7385122d79cc6be444f9120b579f95c5a2343b
mcooganj <mcooganj@gmail.com>
2013-06-13 00:01:11 +1000

    'some more tweaks -- make energy work?'

--- USD/fedVAR/code/fedpVAR.r
+++ USD/fedVAR/code/fedpVAR.r
@@ -21,20 +21,20 @@ dataPATH <- file.path(projPATH, "data")
 plotPATH <- file.path(projPATH, "plot")
 # }}}
 
-getData <- TRUE
+getData <- FALSE
 
 if(getData)
 {
     # data is CF_NAI, cf_fci, adj_cf_fci, unrate,
     # fftarget, upper, lower, 3mTbill, spot oil, gasoline
+    # PPI crude energy index
     dataNames <- c('CFNAI', 'NFCI', 'ANFCI', 'UNRATE', 'PCEPILFE',
                    'DFEDTAR', 'DFEDTARU', 'DFEDTARL', 'WGS3MO', 'OILPRICE',
-                   'GASREGW', 'DCOILBRENTEU'
+                   'GASREGW', 'DCOILBRENTEU', 'PPICEM'
                    )
     getSymbols(dataNames,src='FRED', return.class = 'xts')
-
     save(CFNAI, NFCI, ANFCI, UNRATE, PCEPILFE, DFEDTAR, DFEDTARU, DFEDTARL,
-         WGS3MO, OILPRICE, GASREGW, DCOILBRENTEU,
+         WGS3MO, OILPRICE, GASREGW, DCOILBRENTEU, PPICEM,
          file = file.path(dataPATH, "fedVARdata.rdata"))
 } else {
     load(file = file.path(dataPATH, "fedVARdata.rdata"))
@@ -43,13 +43,13 @@ if(getData)
 FCI <- apply.monthly(merge(NFCI, ANFCI), colMeans)
 index(FCI) <- dateSwitch(index(FCI), lastDay = FALSE)
 
-corePCE_m <- 100*(PCEPILFE / lag(PCEPILFE, 1) - 1)
+corePCE_m <- 100*diff(PCEPILFE, log=TRUE)
 corePCE_y <- 100*(PCEPILFE / lag(PCEPILFE, 12) - 1)
 corePCE_6mAR <- ((1 + rollapplyr(corePCE_m/100, 6, mean))^12 - 1)*100
 names(corePCE_y) <- 'corePCE_y'
 names(corePCE_6mAR) <- 'corePCE_6mAR'
 
-tbill3m <- apply.monthly(WGS3MO, mean, na.rm=T)
+tbill3m <- apply.monthly(WGS3MO, mean, na.rm=TRUE)
 names(tbill3m) <- 'tbill3m'
 index(tbill3m) <- dateSwitch(index(tbill3m), lastDay = FALSE)
 
@@ -59,9 +59,39 @@ LT_tt <- seq(index(first(OILPRICE)), index(last(BRENT_m)), by = 'mon')
 LToil <- xts(rbind(coredata(OILPRICE['::19870401']),
                    coredata(BRENT_m['19870501::'])),
              order.by = LT_tt)
-LToil_m <- 100*(LToil / lag(LToil, 1) - 1)
+LToil_m <- 100*diff(LToil, log=TRUE)
 LToil_y <- 100*(LToil / lag(LToil, 12) - 1)
 LToil_6mAR <- ((1 + rollapplyr(LToil_m/100, 6, mean))^12 - 1)*100
+LToil_3yrMax <- rollapplyr(LToil, 36, max)
+LToil_shock <- xts(pmax(0, log(LToil / lag(LToil_3yrMax,6))), order.by = index(LToil))
+
+PPIenergy <- PPICEM
+names(PPIenergy) <- 'PPIenergy'
+if(index(index(last(PPIenergy)) < index(last(BRENT_m)))) {
+    tt_extnd <- seq(index(first(PPIenergy)), index(last(LToil_m)), by = 'mon')
+    PPIe_act <- coredata(PPIenergy)
+    extndAfter <- dateSwitch(index(last(PPIenergy)), lastDay = FALSE, adv = 1)
+    extndAfter_x <- paste0(extndAfter, "::")
+    extnd_PPIe <- vector(mode= 'numeric', length = length(LToil_m[extndAfter_x]))
+    for (i in seq_along(extnd_PPIe)) {
+        if((i) == 1){
+            extnd_PPIe[i] <- tail(PPIe_act,1) * (1 + coredata(LToil_m[extndAfter_x][i])/100)
+        } else {
+            extnd_PPIe[i] <- extnd_PPIe[(i-1)] * (1 + coredata(LToil_m[extndAfter_x][i])/100)
+        }
+    }
+    PPIe_extnd <- xts(c(PPIe_act, extnd_PPIe), tt_extnd)
+}
+
+gas_3yrmax <- rollapplyr(GASREGW, 36, max)
+gasShock <- xts(pmax(0, log(GASREGW / lag(gas_3yrmax, 6))), order.by = index(GASREGW))
+
+PPIe_3yrMax <- rollapplyr(PPIe_extnd, 36, max)
+PPIe_shock <- xts(pmax(0, log(PPIe_extnd / lag(PPIe_extnd, 6))), order.by = tt_extnd)
+
+# plot(LToil_shock, las=1)
+# lines(PPIe_shock, col=3)
+# lines(gasShock, col=4)
 
 # info crit -- set to FPE
 icT <- "FPE"
@@ -71,8 +101,8 @@ VAR4frame <- cbind(CFNAI, FCI$NFCI, corePCE_6mAR, tbill3m$tbill3m)
 VAR4frame_3m <- rollapplyr(na.locf(VAR4frame), 3, colMeans)['19820301::20090331']
 
 # demand, FCI, inflation, oil, 3m bill
-VAR5frame <- cbind(LToil_6mAR, CFNAI, FCI$NFCI, corePCE_6mAR, tbill3m$tbill3m)
-VAR5frame_3m <- rollapplyr(na.locf(VAR5frame), 3, colMeans)['19820301::20081231']
+VAR5frame <- cbind(PPIe_shock, CFNAI, FCI$NFCI, corePCE_6mAR, tbill3m$tbill3m)
+VAR5frame_3m <- rollapplyr(na.locf(VAR5frame), 3, colMeans)['19820301::20090331']
 
 # ==> TODO -- email the dudes at the chicago fed about the FCI in a VAR ... adj or no?
 
@@ -90,25 +120,26 @@ print(sumTestError.fed4_6m$r)
 # fed4VAR.test12 <- testVar(scale(VAR4frame_3m), skip = 94, nAhead = 12, Vlag = 9, IC = icT)
 # sumTestError.fed4_12m <- errTstVar(fed4VAR.test12)
 # }}} close rmsfe
-fed4VAR.mod <- VAR(scale(VAR4frame_3m), p = optLag, ic = icT)
-fed4VAR.mod2 <- VAR((VAR4frame_3m), p = optLag, ic = icT)
-fed4VAR.pp <- predict(VAR(VAR4frame_3m, p = optLag, ic = icT), n.ahead = 60)
+fed4VAR.mod <- VAR(scale(VAR4frame_3m), p = optLag4, ic = icT)
+fed4VAR.mod2 <- VAR((VAR4frame_3m), p = optLag4, ic = icT)
+fed4VAR.pp <- predict(VAR(VAR4frame_3m, p = optLag4, ic = icT), n.ahead = 60)
 fed4VAR.irf <- irf(fed4VAR.mod2, n.ahead=48)
 # }}} close 4 part VAR
 
 # {{{ five part VAR - add in Oil
 
 optLag5 <- findMaxVARLag(VAR5frame_3m, firstMax = 9, crit = paste0(icT, "(n)")) # 5
+
 # {{{VAR test stuff -- rmsfe etc
 fed5VAR.test6 <- testVar(scale(VAR5frame_3m), skip = 94, nAhead = 6, Vlag = 9, IC = icT)
 sumTestError.fed5_6m <- errTstVar(fed5VAR.test6)
 print(sumTestError.fed5_6m$r)
-#
+
 fed5VAR.test12 <- testVar(scale(VAR5frame_3m), skip = 94, nAhead = 12, Vlag = 9, IC = icT)
 sumTestError.fed5_12m <- errTstVar(fed5VAR.test12)
 # }}} close rmsfe
-fed5VAR.mod <- VAR(scale(VAR5frame_3m), p = optLag, ic = icT)
-fed5VAR.mod2 <- VAR((VAR4frame_3m), p = optLag, ic = icT)
-fed5VAR.pp <- predict(VAR(VAR5frame_3m, p = optLag, ic = icT), n.ahead = 60)
+fed5VAR.mod <- VAR(scale(VAR5frame_3m), p = optLag5, ic = icT)
+fed5VAR.mod2 <- VAR((VAR4frame_3m), p = optLag5, ic = icT)
+fed5VAR.pp <- predict(VAR(VAR5frame_3m, p = optLag5, ic = icT), n.ahead = 60)
 fed5VAR.irf <- irf(fed5VAR.mod2, n.ahead=48)
 # }}} close 5 part VAR
