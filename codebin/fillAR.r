7e98cc9ad7cefac3212f6500996204f5a9fa5056
ricardianambivalence <ricardianambivalence@gmail.com>
2013-10-17 18:13:04 +1100

    'added some helper functions'

--- codebin/fillAR.r
+++ codebin/fillAR.r
@@ -23,6 +23,45 @@ rr <- Reduce(merge, list(px1, px2, px3, px4))
 # you can apply across an xts object like a matrix
 # apply(rr, 2, last, 20)
 
-na.arfill <- function(XTS){
-    which(is.na(px1))[which(is.na(px1)) > 30]
+na.fillAR <- function(XTS){
+# check it's an xts
+    stopifnot(is.xts(XTS))
+# remove leading NAs
+# note for later that 1:(first_nonNA-1) is the leading NA part
+    first_nonNA <- min(which(!is.na(XTS)))
+    XTS_trim <- XTS[first_nonNA:nrow(XTS)]
+# next find location of all non-na rows in the leading and non-leading parts
+    earlyNAs <- which(is.na(XTS_trim))[which(is.na(XTS_trim)) <= 30]
+    deep_NAs <- which(is.na(XTS_trim))[which(is.na(XTS_trim)) > 30]
+# fill initial NA's with some method
+    XTS_trim[1:30] <- na.locf(XTS_trim[1:30])
+}
+
+
+addObvAR <- function(XTS, iter = 1){
+# takes an XTS and returns the next observation as calc'd using AR(1)
+    stopifnot(is.xts(XTS))
+    if(iter == 0) {
+        rbind(XTS, xts())
+    } else {
+        nextObvX <- xts(lm(XTS ~ lag(XTS))$coefficients %*% c(1, last(XTS)),
+                        order.by = nextDate(XTS))
+        XTS <- rbind(XTS, nextObvX)
+        addObvAR(XTS, (iter - 1))
+    }
+}
+
+xPeriodtoMon <- function(XTS){
+    stopifnot(is.xts(XTS))
+    switch(periodicity(XTS)$scale,
+           monthly = 'month',
+           quarterly = ' 3month'
+           )
+}
+
+nextDate <- function(XTS){
+    stopifnot(is.xts(XTS))
+    freq <- xPeriodtoMon(XTS)
+    nextDate <- seq(last(index(XTS)), by = freq, length.out = 2)[2]
+    nextDate
 }
